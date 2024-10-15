9c65372563e4b8c797bd25aa4d5c0e73e863b39f
CarlosEDsl <carloseduardoestds@gmail.com>
2024-10-14 23:25:21 -0300

    Projeto para o minicurso

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import router from './rotas/rotaUsuario';
 import usuarioRouter from './rotas/rotaUsuario';
-import tarefaRouter from './rotas/rotaTarefa';
 const app = express();
 
 const PORT = 8080;
 
 app.use(express.json());
 app.use('/user', usuarioRouter);
-app.use('/task', tarefaRouter)
 
 app.listen(PORT, ()=> console.log("API online na porta: " + PORT));
