c8fa3430ab758d8fbd59d438d902257af4954e91
CarlosEDsl <carloseduardoestds@gmail.com>
2024-10-15 22:06:55 -0300

    Minicurso 3

--- src/repositorios/TarefaRepositorio.ts
+++ src/repositorios/TarefaRepositorio.ts
@@ -1,9 +1,10 @@
 import { Tarefa } from "../modelos/Tarefa";
+import { IdGenerator } from "../utils/idGenerator";
 
 export class TarefaRepositorio {
 
     private static instance:TarefaRepositorio;
-    private Tarefas:Tarefa[] = [];
+    private idGenerator:IdGenerator;
 
     public static getInstance() {
         if(!TarefaRepositorio.instance)
@@ -11,5 +12,42 @@ export class TarefaRepositorio {
         return TarefaRepositorio.instance
     }
 
+    private constructor() {
+        this.idGenerator = new IdGenerator();
+    }
 
+    private Tarefas:Tarefa[] = [];
+
+
+    public create(tarefa:Tarefa):Tarefa {
+        tarefa.id = this.idGenerator.nextId();
+        this.Tarefas.push(tarefa);
+        return tarefa;
+    }
+
+    public find(id:number):Tarefa | undefined {
+        return this.Tarefas.find(tarefa => tarefa.id === id);
+    }
+
+    public findAllByUser(id:number) {
+        return this.Tarefas.filter(tarefa => tarefa.id_usuario === id);
+    }
+
+    public update(tarefa:Tarefa):Tarefa {
+        const index = this.Tarefas.findIndex(valor => valor.id == tarefa.id);
+        if(index === -1) {
+            throw new Error("Tarefa não encontrada");
+        }
+        this.Tarefas[index] = tarefa;
+        return this.Tarefas[index];
+    }
+
+    public delete(id:number):boolean {
+        const index = this.Tarefas.findIndex(valor => valor.id === id);
+        if(index != -1) {
+            this.Tarefas.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
 }
